6cb5520083509ff6c42cb338eb34e589e39f7aec
Jovan Jevtic <jovanjevtic132@gmail.com>
2022-11-15 22:50:04 +0100

    Cors

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middlewares/errorMiddleware');
+const cors = require('cors');
+const app = express();
+
+const allowedOrigins = ['https://elibrary-94692.web.app', 'https://elibrary-94692.firebaseapp.com', 'http://localhost:8100']
+
+app.use(cors({
+    origin: allowedOrigins
+}))
 
 connectDB();
-const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
