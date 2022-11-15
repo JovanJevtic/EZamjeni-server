355e81dfa6bd498025d1fa91fdced1fe8555f67c
Jovan Jevtic <jovanjevtic132@gmail.com>
2022-11-15 21:31:07 +0100

    User Login

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -49,6 +49,22 @@ const registerUser = asyncHandler( async (req, res) => {
 })
 
 //? Login User, post to '/login'
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid credentials');
+  }
+});
 
 //? Get my User, get to '/me'
 
@@ -57,5 +73,6 @@ const registerUser = asyncHandler( async (req, res) => {
 //? Get User by ID, get to '/:id'
 
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser } = require('../controllers/userControllers');
+const { registerUser, loginUser } = require('../controllers/userControllers');
 
 router.post('/', registerUser);
-// router.post('/login');
+router.post('/login', loginUser);
 // router.get('/confirmation/:email/:token', confirmEmail);
 // router.post('/confirmation/resendEmail', resendEmail );
 // router.put('/me')
